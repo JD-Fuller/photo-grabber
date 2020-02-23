98c6b4da312a782615059fdbeefcc984717c95ed
JD-Fuller <jdfuller@live.com>
2020-02-22 19:16:59 -0700

    built out image results

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import NavBar from "./Components/navbar/NavBar";
+import Search from "./Components/search/Search";
 import "./App.css";
 
 class App extends Component {
   render() {
-    <MuiThemeProvider>
-      <div>
-        <h1>This is just above the Navbar</h1>
-        <NavBar />
-        <h1>This is just below the Navbar</h1>
-      </div>
-    </MuiThemeProvider>;
+    return (
+      <MuiThemeProvider>
+        <div>
+          <h1>This is just above the Navbar</h1>
+          <NavBar />
+          <Search />
+          <h1>This is just below the Navbar</h1>
+        </div>
+      </MuiThemeProvider>
+    );
   }
 }
 
---
+++ src/Components/image-results/ImageResults.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { GridList, GridTile } from "material-ui/GridList";
+import IconButton from "material-ui/IconButton";
+import ZoomIn from "material-ui/svg-icons/action/zoom-in";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+
+class ImageResults extends Component {
+  state = {
+    open: false,
+    currentImg: ""
+  };
+
+  handleOpen = img => {
+    this.setState({ open: true, currentImg: img });
+  };
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+  render() {
+    //to ensure images load, only when there are images
+    let imageListContent;
+    const { images } = this.props;
+
+    if (images) {
+      imageListContent = (
+        //map through images on cols
+        <GridList cols={3}>
+          {images.map(img => (
+            <GridTile
+              title={img.tags}
+              key={img.id}
+              subtitle={
+                <span>
+                  by <strong>{img.user}</strong>
+                </span>
+              }
+              actionIcon={
+                <IconButton>
+                  <ZoomIn color="white" />
+                </IconButton>
+              }
+            >
+              <img src={img.largeImageURL} alt="" />
+            </GridTile>
+          ))}
+        </GridList>
+      );
+    } else {
+      imageListContent = null;
+    }
+    const actions = [
+      <FlatButton label="Close" primary={true} onClick={this.handleClose} />
+    ];
+    //this returns the image list
+    return <div>{imageListContent}</div>;
+  }
+}
+
+ImageResults.prototypes = {
+  images: PropTypes.array.isRequired
+};
+export default ImageResults;
